c7701ea3a40fa79e7970c9b0c921da8911cdcee2
Ian Abreu <ianabreudev@gmail.com>
2025-01-01 15:55:34 -0300

    add Badge component

---
+++ src/components/Badge/index.tsx
@@ -0,0 +1,27 @@
+import { Pressable, PressableProps, View } from "react-native";
+import { Typography } from "../Typography";
+import { styles } from "./styles";
+import { theme } from "@/src/theme";
+
+interface BadgeProps extends PressableProps {
+  text: string;
+  type: "genre" | "platform";
+}
+export default function Badge({ text, type, ...rest }: BadgeProps) {
+  return (
+    <Pressable
+      style={[
+        styles.defaultStyles,
+        {
+          backgroundColor:
+            type === "genre" ? theme.colors.badge : theme.colors.shape,
+        },
+      ]}
+      {...rest}
+    >
+      <Typography variant={type === "genre" ? "bold" : "body"}>
+        {text}
+      </Typography>
+    </Pressable>
+  );
+}
---
+++ src/components/Badge/styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  defaultStyles: {
+    padding: 8,
+    borderRadius: 8,
+  },
+});
